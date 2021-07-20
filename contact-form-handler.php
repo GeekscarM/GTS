a16e2145b5858d6d3d97e056a3d08f01be366ca5
Oscar Rodriguez <geekscar@users.noreply.github.com>
2021-07-20 11:07:41 -0500

    Update contact-form-handler.php
    Update form handler php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,19 +1,20 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-$email_from = 'geekscar.tech.service@gmail.com';
-$email_subject = "New Form Submission";
-$email_body = "Usuario: $name.\n".
-                "Correo: $visitor_email.\n".
-                    "Mensaje: $message.\n";
+    $email_from = 'geekscar.tech.service@gmail.com';
+    $email_subject = "New Form Submission";
+    $email_body = "Usuario: $name.\n".
+                    "Correo: $visitor_email.\n".
+                        "Mensaje: $message.\n";
 
-$to = "geekscar.tech.service@gmail.com";
+    $to = "geekscar.tech.service@gmail.com";
 
-$headers = "De: $email_from \r\n";
-$headers .= "Responder-a: $visitor_email \r\n";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Replay-to: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+    mail($to,$email_subject,$email_body,$headers);
 
-header("Location: index.html");
+    header("Location: index.html");
+?>
