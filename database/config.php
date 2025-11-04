e8418d48689d981ba629bff586c91a7cf4b680f6
GeekscarM <oscar.rodriguez.o@outlook.com>
2025-11-04 09:47:41 -0600

    Rollback to GTS

--- database/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    /*$conn = mysqli_connect('localhost','root','','bjx1_cafeteria') or die('connection failed');*/
-    $conn = mysqli_connect('localhost','bjx1-it-dev','Amzn10!!','bjx1_WHS_VisitLog') or die('connection failed');         
-?>
